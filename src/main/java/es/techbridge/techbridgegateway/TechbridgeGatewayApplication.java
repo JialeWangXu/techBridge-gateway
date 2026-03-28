e0bbd44ac60030200faf8449d2f17894b1bfdad0
JialeWangXu <jiale.wang@alumnos.upm.es>
2026-03-28 21:23:45 +0100

    Initial commit techBridge-gateway

---
+++ src/main/java/es/techbridge/techbridgegateway/TechbridgeGatewayApplication.java
@@ -0,0 +1,15 @@
+package es.techbridge.techbridgegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class TechbridgeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechbridgeGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/techbridge/techbridgegateway/TechbridgeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package es.techbridge.techbridgegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechbridgeGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
